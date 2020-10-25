680c58f3ef920e869fd1707e58c59e541babb3f5
hiromichi-code <hiromichi.s.617@gmail.com>
2020-10-25 21:00:32 +0900

    first blog

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <a href="https://twitter.com/PlogTwi"><span>Twitter</span></a>
+          <a href="https://twitter.com/PlogTwi"><span>Twitter</span></a>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
